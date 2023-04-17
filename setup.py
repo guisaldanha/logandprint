b74953c21646730280aedf927309cd789cd3b41b
guisaldanha <guisaldanha@gmail.com>
2023-04-16 21:42:19 -0300

    Chore: workflow and long description

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 import setuptools
 
-version = '1.0.0'
-
-with open("README.md", "r") as fh:
-    longDescription = fh.read()
+version = '1.0.1'
 
 setuptools.setup(
     name = "logandprint",
@@ -11,7 +8,7 @@ setuptools.setup(
     author = "Guilherme Saldanha",
     author_email = "guisaldanha@gmail.com",
     description = "A simple logging package that helps you to log what is happening in your application.",
-    long_description = longDescription,
+    long_description = "This logging package is a straightforward yet powerful tool that assists developers in logging the activities occurring within their application. It provides a user-friendly interface and customizable settings, enabling developers to generate comprehensive logs that document events and actions within their application. This package is an essential tool for developers who want to troubleshoot issues, improve performance, and optimize the user experience",
     long_description_content_type = "text/markdown",
     license_files = ('LICENSE',),
     url = "https://github.com/guisaldanha/logandprint",
